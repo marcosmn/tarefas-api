0926d2222940663fab25987084a5fb4323b20475
Leonardo MINORA <leonardo.minora@gmail.com>
2018-10-19 16:23:19 -0300

    atualizando para adaptar ao heroku

--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,15 @@ module.exports = {
       filename: "./mydb.sqlite",
       charset: "utf8"
     }
+  },
+  production: {
+    migrations: { tableName: "knex_migrations" },
+    seeds: { tableName: "./seeds" },
+
+    client: "sqlite3",
+    connection: {
+      filename: "./mydb.sqlite",
+      charset: "utf8"
+    }
   }
 };
