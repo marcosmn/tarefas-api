a68f6cf6c909c720869a6953a8434d99c2fc4488
Marcos Martins <30351698+marcosmn@users.noreply.github.com>
2018-12-16 22:07:37 -0300

    Atualizando para receber a rota de usuário

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import Hapi from "hapi";
 import knex from "./config/knew";
 
-import { root, tasks } from "./routes";
+import { root, tasks, users } from "./routes";
 
 const server = new Hapi.Server({
   port: process.env.PORT || 8000
 });
 
 const init = async () => {
-  server.route([root, tasks]);
+  server.route([root, tasks, users]);
 
   // server.start();
   await server.start();
